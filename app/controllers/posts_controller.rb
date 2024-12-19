15b2f6c492bf5512e7ba24052388d992e3615da6
Chap Ambrose <cambrose@salesforce.com>
2024-12-19 10:17:12 +0000

    bump

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -71,9 +71,9 @@ class PostsController < ApplicationController
     end
 
     def cors
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-      headers['Access-Control-Request-Method'] = '*'
-      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      headers["Access-Control-Allow-Origin"] = "*"
+      headers["Access-Control-Allow-Methods"] = "POST, PUT, DELETE, GET, OPTIONS"
+      headers["Access-Control-Request-Method"] = "*"
+      headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     end
 end
